c974ba0a1ab1ee4ef22ac586e49f89751a55d3b1
Piotr Słomka <piotrslomka12@gmail.com>
2025-10-25 21:15:31 +0200

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [NavbarComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
